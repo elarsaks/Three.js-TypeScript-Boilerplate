49b41473af56327a640ea7dcfa79623e6640e0a0
Elar Saks <elarsaks@gmail.com>
2022-12-05 21:41:48 +0200

    Add dat GUI

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,8 +1,9 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
-
+scene.background = new THREE.Color(0x000000)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 2
 
@@ -19,7 +20,12 @@ const material = new THREE.MeshBasicMaterial({
 })
 
 const cube = new THREE.Mesh(geometry, material)
+cube.scale.x = 0.7
+cube.scale.y = 0.7
+cube.scale.z = 0.7
 scene.add(cube)
+
+console.dir(scene)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -29,11 +35,23 @@ function onWindowResize() {
     render()
 }
 
+const gui = new GUI()
+const cubeFolder = gui.addFolder('Cube')
+cubeFolder.open()
+gui.add(cube.scale, 'x', 0, Math.PI * 2)
+gui.add(cube.scale, 'y', 0, Math.PI * 2)
+gui.add(cube.scale, 'z', 0, Math.PI * 2)
+
+const camerFolder = gui.addFolder('Camera')
+camerFolder.add(camera.position, 'z', 0, 20)
+camerFolder.open()
+
 function animate() {
     requestAnimationFrame(animate)
 
     cube.rotation.x += 0.01
     cube.rotation.y += 0.01
+    cube.rotation.z += 0.01
 
     controls.update()
 
